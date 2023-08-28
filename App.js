6433a27603cb4af7a9af7ab6959bf791dfc2c26a
swdev <ddguru777@gmail.com>
2023-08-27 23:01:51 -0400

    update android target sdk 33

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import {StyleSheet, View, Platform, Dimensions} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  Dimensions,
+  SafeAreaView,
+  Linking,
+  AppState,
+  ActivityIndicator,
+} from 'react-native';
 import {WebView} from 'react-native-webview';
-// import SplashScreen from 'react-native-smart-splash-screen';
 import DeviceInfo from 'react-native-device-info';
+import {
+  getTrackingStatus,
+  requestTrackingPermission,
+} from 'react-native-tracking-transparency';
+
+const DOMAIN_URL = 'https://sba-secure.dwellant.com';
+const SIGNIN_URL = DOMAIN_URL + '/Account/SignInOrRegister';
 
 export default function SplashView() {
   const dim = Dimensions.get('window');
 
-  const [state, setstate] = React.useState({
+  const [state, setState] = useState({
     webviewDisplay: 'flex',
     deviceId: '',
     shouldAddPaddingTopForiPhone: dim.width < dim.height && true,
   });
+  const [jsCode, setJsCode] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
-  const {webviewDisplay, shouldAddPaddingTopForiPhone, deviceId} = state;
+  const {webviewDisplay, deviceId} = state;
 
-  React.useEffect(() => {
-    // if (Platform.OS === 'android') {
-    //   SplashScreen.close({
-    //     animationType: SplashScreen.animationType.scale,
-    //     duration: 850,
-    //     delay: 500,
-    //   });
-    // }
-
+  useEffect(() => {
     // Check device orientation for iOS
     Dimensions.addEventListener('change', () => {
-      setstate({
+      setState({
         ...state,
         shouldAddPaddingTopForiPhone: dim.width < dim.height && true,
       });
@@ -35,82 +44,96 @@ export default function SplashView() {
     // Getting the Unique Id from here
     if (!state.deviceId) {
       if (Platform.OS === 'ios') {
-        setstate({...state, deviceId: DeviceInfo.getUniqueId()});
+        setState({...state, deviceId: DeviceInfo.getUniqueId().toString()});
       } else if (Platform.OS === 'android') {
         DeviceInfo.getAndroidId().then(id => {
-          setstate({...state, deviceId: id.toString()});
+          setState({...state, deviceId: id.toString()});
         });
       }
     }
   }, [dim.height, dim.width, state]);
 
-  const navChanged = () => {};
+  useEffect(() => {
+    const expiryDate = new Date();
+    expiryDate.setTime(expiryDate.setDate(expiryDate.getDate() + 1));
+    const code = `(function () {document.cookie = 'deviceId=${deviceId};expires=${expiryDate};path=/;';})();`;
 
-  const onMessage = () => {
-    // const { data } = event.nativeEvent;
+    if (Platform.OS === 'ios') {
+      const listener = AppState.addEventListener('change', status => {
+        if (status === 'active') {
+          (async () => {
+            const trackingStatus = await getTrackingStatus();
+            if (trackingStatus === 'not-determined') {
+              const retStatus = await requestTrackingPermission();
+              if (retStatus === 'authorized') {
+                // enable tracking features
+                setJsCode(code);
+              }
+            }
+          })();
+        }
+      });
+      return () => {
+        listener && listener.remove();
+      };
+    } else {
+      setJsCode(code);
+    }
+  }, [deviceId]);
+
+  const externalLinkHandler = event => {
+    if (!event.url.includes(DOMAIN_URL)) {
+      Linking.openURL(event.url);
+      return false;
+    }
+    return true;
   };
-  function isIPhoneXAndAbove() {
-    return shouldAddPaddingTopForiPhone
-      ? dim.height === 812 || dim.height === 896 || dim.height === 926
-      : false;
-  }
 
-  const expiryDate = new Date();
-  expiryDate.setTime(expiryDate.setDate(expiryDate.getDate() + 1));
-  const jsCode = `(function () {document.cookie = 'deviceId=${deviceId};expires=${expiryDate};path=/;';})();`;
   return (
-    <>
-      {Platform.OS === 'ios' && (
-        <View
-          style={[
-            styles.container,
-            {
-              paddingTop: isIPhoneXAndAbove() ? 52 : 22,
-              backgroundColor: 'white',
-              zIndex: 1,
-            },
-          ]}>
-          <WebView
-            source={{
-              uri: 'https://sba-secure.dwellant.com/Account/SignInOrRegister',
-            }}
-            style={[styles.WebViewStyle, {display: webviewDisplay, zIndex: 2}]}
-            javaScriptEnabled
-            domStorageEnabled
-            startInLoadingState
-            onNavigationStateChange={navChanged}
-            injectedJavaScript={jsCode}
-          />
+    <View style={styles.container}>
+      {isLoading && (
+        <View style={styles.activityContainer}>
+          <ActivityIndicator size="large" color="rgba(144, 112, 44, 1.0)" />
         </View>
       )}
-      {Platform.OS === 'android' && (
-        <View style={styles.container}>
-          <WebView
-            source={{
-              uri: 'https://sba-secure.dwellant.com/Account/SignInOrRegister',
-            }}
-            style={[styles.WebViewStyle, {display: webviewDisplay}]}
-            javaScriptEnabled
-            domStorageEnabled
-            startInLoadingState
-            onNavigationStateChange={navChanged}
-            onMessage={onMessage}
-            injectedJavaScript={jsCode}
-          />
-        </View>
-      )}
-    </>
+      {Platform.OS === 'ios' && <SafeAreaView />}
+      <WebView
+        source={{
+          uri: SIGNIN_URL,
+        }}
+        onLoad={() => {
+          setLoading(false);
+        }}
+        style={[styles.WebViewStyle, {display: webviewDisplay}]}
+        javaScriptEnabled
+        domStorageEnabled
+        injectedJavaScript={jsCode}
+        onShouldStartLoadWithRequest={externalLinkHandler}
+        allowsBackForwardNavigationGestures={true}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f4ef',
+    backgroundColor: '#ebe9df',
     position: 'relative',
   },
   WebViewStyle: {
     backgroundColor: 'transparent',
     flex: 1,
+  },
+  activityContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    height: '100%',
+    width: '100%',
+    zIndex: 9,
   },
 });
